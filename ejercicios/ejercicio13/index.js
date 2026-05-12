f785ff3570ef5d7f40db6a63c1a3129c7cd44266
Asdiz10 <sanchezelmoussouiadam@fpllefia.com>
2026-05-12 13:26:22 +0200

    Seguimos en casa ANIMO!!!

---
+++ ejercicios/ejercicio13/index.js
@@ -0,0 +1,27 @@
+let alumno = [
+{
+  infodavid:
+    "David es un alumno creativo poco sociable, no le gusta la multitud, pero saca notas decentes. Le gusta la profesora de programación ",
+
+  imagen:"",
+
+  NOTAS:
+    "",
+},
+
+{
+    
+  infojose:
+    "Charmander es un Pokémon bípedo de color naranja, originario de zonas calientes y montañosas. Se dice que desde su nacimiento, una llama arde en la punta de su cola como indicador de su salud y fuerza vital.",
+
+  imagen:
+    "https://static.wikia.nocookie.net/doblaje/images/d/de/Charmander_Anime.png/revision/latest/scale-to-width/360?cb=20250119211347&path-prefix=es",
+
+  características:
+    "Tipo: Fuego | Habilidad: Mar Llamas | Debilidad: Agua, Tierra y Roca.",
+},
+
+
+]
+
+
