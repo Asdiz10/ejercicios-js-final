dbb1b828431192193b67719d57bf30e3b2c7cc13
Asdiz <sanchezelmoussaouiadam@fpllefia.com>
2026-05-23 17:33:49 +0200

    seguimos

--- ejercicios/ejercicio15/index.js
+++ ejercicios/ejercicio15/index.js
@@ -12,5 +12,15 @@ let miDiv1 = document.querySelector("#div1")
 function hola(){
     miDiv1.innerHTML = ` Cragando datos...
     `
+    miDiv1.classList.add("resaltado")
 }
 btn2.addEventListener("click", hola)
+
+
+const btn3 = document.querySelector("#boton3")
+
+function hola1(){
+    miDiv1.innerHTML = " "
+    miDiv1.classList.add("resaltado")
+}
+btn3.addEventListener("click", hola1)
