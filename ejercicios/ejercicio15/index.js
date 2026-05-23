24400234afa3293f3c57d0e54c9c00f39a22b889
Asdiz <sanchezelmoussaouiadam@fpllefia.com>
2026-05-23 17:25:57 +0200

    VAMOS!!!

--- ejercicios/ejercicio15/index.js
+++ ejercicios/ejercicio15/index.js
@@ -4,3 +4,13 @@ function alerta(){
     console.log("Sistema Iniciado")
 }
 btn.addEventListener("click", alerta)
+
+
+const btn2 = document.querySelector("#boton2")
+let miDiv1 = document.querySelector("#div1")
+
+function hola(){
+    miDiv1.innerHTML = ` Cragando datos...
+    `
+}
+btn2.addEventListener("click", hola)
