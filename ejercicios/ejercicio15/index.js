e4616981968f5261aeb68f39ef01b6b515e57604
Asdiz <sanchezelmoussaouiadam@fpllefia.com>
2026-05-24 15:58:57 +0200

    ejercicio de array

--- ejercicios/ejercicio15/index.js
+++ ejercicios/ejercicio15/index.js
@@ -31,9 +31,50 @@ let divv = document.querySelector("#div2")
 let selec = document.querySelector("#sele")
 let ipt1 = document.querySelector("#input")
 
+let divvv = document.querySelector("#div3")
+let ipt2 = document.querySelector("#input2")
+
 function hola2(){
     divv.innerHTML = ` " ${ipt1.value} - ${selec.value} "
     `
-
+    divvv.innerHTML = ` <img src="${ipt2.value}">
+    `
 }
 btn4.addEventListener("click", hola2)
+
+
+const videojuegos = [
+  { id: 1, titulo: "Zelda", genero: "Aventura", precio: 60, stock: 5 },
+
+  { id: 2, titulo: "FIFA 24", genero: "Deporte", precio: 70, stock: 2 },
+
+  { id: 3, titulo: "Doom", genero: "Acción", precio: 30, stock: 0 },
+
+  { id: 4, titulo: "Minecraft", genero: "Creativo", precio: 20, stock: 10 }
+];
+
+
+
+
+const btn5 = document.querySelector("#boton5")
+let container = document.querySelector("#contenedor")
+
+function pintarjuegos(){
+    container.innerHTML="";
+    for (let i = 0; i < videojuegos.length; i++){
+        
+        container.innerHTML += `
+
+            <div class="tarjetas">
+            <h3>${videojuegos[i].titulo}</h3>
+            <p>Género: ${videojuegos[i].genero} </p>
+            <p>Precio: ${videojuegos[i].precio}</p>
+            <p>Disponibles: ${videojuegos[i].stock}</p>
+            </div>
+
+
+
+        `
+    }
+}
+btn5.addEventListener("click", pintarjuegos)
